cd9f916b20cf24e29f99fc86ee16175d3589e495
williner.martin <williner.martin@gmail.com>
2024-08-14 14:22:29 +0000

    cartContext y cartReducer agregado

---
+++ src/context/cartReducer.ts
@@ -0,0 +1,50 @@
+export const initialState = {
+    cartItems: [],
+}
+
+export const cartReducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_TO_CART":
+            const { id } = action.payload;
+
+            // Validar si es el primer ítem del carrito o agregar uno más a algo que ya existe
+            const existingItem = state.cartItems.find((item) => item.id === id);
+
+            if (existingItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map((item) =>
+                        item.id === id ? { ...existingItem, quantity: existingItem.quantity + 1 } : item
+                    ),
+                };
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, action.payload],
+                };
+            }
+        
+        case "REMOVE_FROM_CART":
+            const { id: removeItemId } = action.payload;
+            
+            // Validar si el ítem existe en el carrito
+            const itemToRemove = state.cartItems.find((item) => item.id === removeItemId);
+
+            if (itemToRemove.quantity === 1) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.filter((item) => item.id !== removeItemId),
+                };
+            } else {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map((item) => 
+                        item.id === removeItemId ? { ...itemToRemove, quantity: itemToRemove.quantity - 1 } : item
+                    ),
+                };
+            }
+
+        default:
+            return state;
+    }
+};
