bb443c9a789ffe8bf0b967e9b15d112a8560e054
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-08-13 21:27:03 -0300

    React Router agregado

---
+++ src/components/Layouts/LayoutMain.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+
+
+
+export const LayoutMain = () => {
+  return (
+    <div>
+        Layoutmain
+        <Outlet />
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { LayoutMain } from './components/Layouts/LayoutMain.tsx'
+import {Home} from './pages/Home/Home.tsx'
+import {Cart} from './pages/Cart/Cart.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutMain />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
