525c10d7be3e33948aa875f2b7d86a437f5a4e67
Martin Ezequiel Williner <111790529+martuwilson@users.noreply.github.com>
2024-08-14 01:28:51 -0300

    ModalCart added

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,8 +1,17 @@
 import Logo from '../../assets/logo.svg';
 import Cart from '../../assets/cart.svg';
 import styles from './Navbar.module.css';
+import { useState } from 'react';
+import { CartModal } from './CartModal';
 
 export const Navbar = () => {
+
+    const [showCartModal, setShowCartModal] = useState(false)
+
+
+    const handleShowCartModal = () => {
+        setShowCartModal(!showCartModal)
+    }
 
     return(
         <div className={styles.navbarContainer}>
@@ -17,8 +26,11 @@ export const Navbar = () => {
 
            <div className={styles.navbarCartContainer}>
                 <p className={styles.navbarTextAmount}>2</p>
-                <img src={Cart} alt="cart" />
+                <img src={Cart} alt="cart" onClick={handleShowCartModal}/>
            </div>
+           {
+            showCartModal && (<CartModal handleShowCartModal={handleShowCartModal}/>)
+           }
         </div>
     )
 }
