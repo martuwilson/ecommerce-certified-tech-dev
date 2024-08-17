18f604a959b1588b2bef56a9c0bbcf4a7ca4c717
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-08-17 13:04:08 -0300

    credit card config and toaster for error message

--- src/components/UI/CardCredit/CardCredit.tsx
+++ src/components/UI/CardCredit/CardCredit.tsx
@@ -1,42 +1,97 @@
 import Cards from 'react-credit-cards-2';
 import 'react-credit-cards-2/dist/es/styles-compiled.css';
 import styles from './CardCredit.module.css'
+import { useState } from 'react';
+import { toast } from 'sonner';
 
 
 export const CardCredit = () => {
+
+    const [cardData, setCardData] = useState({
+        number: '',
+        name: '',
+        expiry: '',
+        cvc: '',
+        focus: ''
+    })
+
+    const {number, name, expiry, cvc} = cardData;
+
+    const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setCardData({
+            ...cardData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleInputFocus = (e:React.FocusEvent<HTMLInputElement>) => {
+        setCardData({
+            ...cardData,
+            focus: e.target.name
+        })
+    } 
+
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        //validar campos que no esten vacios
+
+        if([number, name, expiry, cvc].includes('')){
+            toast.error('All fields are required')
+            return
+        }
+
+        //limpiar el estado
+        setCardData({
+            number: '',
+            name: '',
+            expiry: '',
+            cvc: '',
+            focus: ''
+        })
+    }
+
   return (
     <div className={styles.container}>
         <div>
             {/* cards */}
             <Cards
-                number={'1234 5678 1234 5678'}
-                name={'Card Owner Name'}
-                expiry={'12/34'}
-                cvc={'123'}
-                focused={''}
+                number={number}
+                name={name}
+                expiry={expiry}
+                cvc={cvc}
+                focused={
+                    cardData.focus as 'number' | 'name' | 'expiry' | 'cvc'
+                }
             />
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className={styles.formControl}>
                 <label htmlFor="number">Card Number</label>
-                <input type="text" name="number" id="number"/>
+                <input type="text" name="number" id="number" value={number} onChange={handleInputChange} onFocus={handleInputFocus}/>
             </div>
             <div className={styles.formControl}>
                 <label htmlFor="name">Card Owner Name</label>
-                <input type="text" name="name" id="name"/>
+                <input type="text" name="name" id="name" value={name} onChange={handleInputChange} onFocus={handleInputFocus}/>
             </div>
 
             {/* grupo */}
             <div className={styles.formInputGrup}>
                 <div className={styles.formControl}>
                     <label htmlFor="expiry">Card Expiracy</label>
-                    <input type="text" name="expiry" id="expiry"/>
+                    <input type="text" name="expiry" id="expiry" value={expiry} onChange={handleInputChange} onFocus={handleInputFocus}/>
                 </div>
                 <div className={styles.formControl}>
                     <label htmlFor="cvc">CVV</label>
-                    <input type="text" name="cvc" id="cvc"/>
+                    <input type="text" name="cvc" id="cvc" value={cvc} onChange={handleInputChange} onFocus={handleInputFocus}/>
                 </div>
             </div>
+
+            
+                <button type='submit' className={styles.buyButton}>
+                    Buy now
+                </button>
+            
         </form>
     </div>
   )
--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,10 +1,12 @@
 import { CardCredit } from "../../components/UI/CardCredit/CardCredit"
 import { Table } from "../../components/UI/Table/Table"
 import styles from './Checkout.module.css'
+import { Toaster } from 'sonner';
 
 export const Checkout = () => {
   return (
     <div className={styles.container}>
+        <Toaster richColors visibleToasts={1}/>
         <h1 className={styles.title}>
             Checkout
         </h1>
@@ -17,11 +19,7 @@ export const Checkout = () => {
         <CardCredit/>
             </div>
         </div>
-            <div>
-                <button className={styles.buyButton}>
-                    Buy now
-                </button>
-            </div>
+            
         
     </div>
   )
