cb81871a1f99033b3c9e231da45abc0a434b5c1f
Martin Ezequiel Williner <111790529+martuwilson@users.noreply.github.com>
2024-08-18 02:53:45 -0300

    pagination added with useQuery

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-//import { useEffect, useState } from "react"
 import { Hero } from "../../components/UI/Hero/Hero"
 import styles from './Home.module.css'
 import { CardProduct } from "../../components/CardProduct"
@@ -6,31 +5,33 @@ import { getProducts } from "../../service"
 //import { Product } from "../../interfaces"
 import { Toaster } from "sonner"
 import { useQuery } from "react-query"
+import { useEffect, useState } from "react"
 
 
 export const Home = () => {
- const {data, isLoading, error} = useQuery('products', getProducts) // get products es la funcion que trae los products desde la API, 'products' es el nombre de la query
 
- /*  const [products, setProducts] = useState<Product[]>([])
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true)
+  const [page, setPage] = useState(1);
+
+  console.log('current page', page)
+
+  const { data, isLoading, error, refetch } = useQuery(
+    ['products', page],
+    () => getProducts(page),
+    { keepPreviousData: true,
+      staleTime: 0,
+      cacheTime: 0
+    }
+  ) // get products es la funcion que trae los products desde la API, 'products' es el nombre de la query
 
   useEffect(() => {
-    getProducts().then((data) => {
-      setProducts(data)
-      setIsLoading(false)
-    }).catch(() => {
-      setError(true)
-      
-    }).finally(() =>{
-      setIsLoading(false)
-    })
-  }, []) */
+    refetch(); // <-- Forza la revalidación cuando cambia `page`
+  }, [page, refetch]);
+  console.log('data', data)
 
   return (
     <>
       <Hero />
-      <Toaster richColors/>
+      <Toaster richColors />
       {
         isLoading && <p>Loading...</p>
       }
@@ -39,10 +40,24 @@ export const Home = () => {
       }
       <div className={styles.container}>
         {
-          data?.map((product) => (
+          data?.data.map((product) => (
             <CardProduct key={product.tail} product={product} />
           ))
         }
+      </div>
+      <div className={styles.paginationContainer}>
+        <button
+          onClick={() => setPage(page - 1)}
+          disabled={page === 1}
+          className={styles.paginationButton}
+        >Prev Page</button>
+        <div className={styles.paginationActive}>
+          <span>{page}</span>
+        </div>
+        <button
+          onClick={() => setPage(page + 1)}
+          className={styles.paginationButton}
+        >Next Page</button>
       </div>
     </>
   )
--- src/service/products.service.ts
+++ src/service/products.service.ts
@@ -1,17 +1,17 @@
-import {Products} from '../interfaces';
+import {Product} from '../interfaces';
 
-export const getProducts = async ():Promise<Products[]> => {
-    try {
-      const response = await fetch('http://localhost:3000/products')
+export const getProducts = async (page = 1): Promise<Product[]> => {
+  try {
+    const response = await fetch(`http://localhost:3000/products?_page=${page}&_per_page=25}`);
 
-      if(response.ok){
-        const data = await response.json()
-        return data;
-      }else{
-        throw new Error('Error fetching products')
-      }
-
-    } catch (error) {
-      throw new Error('Network error: ' + error)
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error('Error fetching products');
     }
+  } catch (error) {
+    throw new Error('Network error: ' + error);
   }
+};
+
